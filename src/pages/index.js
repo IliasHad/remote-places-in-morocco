7ddc7ff5b2d5f4faf0e23ff15b5a842b1967ca99
iliashad <iliasshaddad18@gmail.com>
2021-02-24 16:35:52 +0100

    Change the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,130 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout"
-import heroImage from "../images/hero-image.svg"
+import SEO from "../components/SEO"
+import ListItem from "../components/listItem"
+import { graphql } from "gatsby"
+export default ({ data, pageContext }) => {
+  const [cities, setCities] = useState([])
+  const [places, setPlaces] = useState([])
+  const [sorting, setSorting] = useState("upload-speed")
+  const [selectedCity, setSelectedCity] = useState("All")
+  useEffect(() => {
+    setCities(data.allAirtable.edges.map(({ node }) => node.data.City))
+  }, [])
 
-export default () => {
+  useEffect(() => {
+    let selectedPlaces =
+      selectedCity === "All"
+        ? data.allAirtable.edges.map(({ node }) => node.data)
+        : data.allAirtable.edges
+            .filter(({ node }) => node.data.City === selectedCity)
+            .map(({ node }) => node.data)
+
+    if (sorting === "upload-speed") {
+      selectedPlaces = selectedPlaces.sort(function (a, b) {
+        return b.Upload_Speed - a.Upload_Speed
+      })
+    } else if (sorting === "download-speed") {
+      selectedPlaces = selectedPlaces.sort(function (a, b) {
+        return b.Download_Speed - a.Download_Speed
+      })
+    } else {
+      selectedPlaces = selectedPlaces.sort(function (a, b) {
+        return b.Rating - a.Rating
+      })
+    }
+
+    setPlaces(selectedPlaces)
+  }, [selectedCity, sorting])
+
   return (
-    <Layout>
-      <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
-        <div className="sm:flex sm:flex-row-reverse sm:items-center">
-          <div className="sm:px-2">
-            <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
-              Gatsby Starter Tailwind CSS
-            </h1>
-            <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8">
-              Start your Gatsby and Tailwind CSS project easily with this Gatsby
-              Starter
-            </p>
+    <>
+      <SEO title="Remote Places In Morocco" />
+      <Layout>
+        <div class="w-1/5 gap-8 py-4 flex">
+          <div>
+            <label
+              for="location"
+              class="block text-sm font-medium text-gray-700"
+            >
+              City
+            </label>
+            <select
+              onChange={e => setSelectedCity(e.target.value)}
+              id="location"
+              name="location"
+              class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              <option value="All" select={selectedCity === "All"}>
+                All Cities
+              </option>
+              {cities.map(city => (
+                <option value={city} select={selectedCity === city}>
+                  {city}
+                </option>
+              ))}
+            </select>
           </div>
-          <img
-            className="w-full max-w-lg mt-16 mx-auto sm:w-1/2"
-            src={heroImage}
-            alt="Gatsby and Tailwind CSS together"
-          />
-        </div>
 
-        <div className="mx-auto">
-          <a
-            href="https://github.com/melanienolan/gatsby-starter-tailwind-css"
-            className="p-4 bg-teal-500 text-white text-xs rounded shadow hover:bg-teal-400"
-          >
-            View on GitHub
-          </a>
+          <div>
+            <label
+              for="location"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Sorting
+            </label>
+            <select
+              onChange={e => setSorting(e.target.value)}
+              id="location"
+              name="location"
+              class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              <option
+                value="upload-speed"
+                selected={sorting === "upload-speed"}
+              >
+                Upload Speed
+              </option>
+              <option
+                value="download-speed"
+                selected={sorting === "download-speed"}
+              >
+                Download Speed
+              </option>
+              <option value="rating" selected={sorting === "rating"}>
+                Rating
+              </option>
+            </select>
+          </div>
         </div>
-
-        <div className="flex items-center p-3 mx-2 bg-white rounded shadow-xs sm:mx-auto">
-          <code className="text-gray-600 text-xs leading-xl">
-            gatsby new my-project-name
-            https://github.com/melanienolan/gatsby-starter-tailwind-css
-          </code>
-        </div>
-      </main>
-    </Layout>
+        <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {places.length > 0 && places.map(node => <ListItem node={node} />)}
+        </ul>
+      </Layout>
+    </>
   )
 }
+
+export const query = graphql`
+  query {
+    allAirtable(filter: { data: { Approved: { eq: true } } }) {
+      edges {
+        node {
+          data {
+            City
+            Curated_By
+            Download_Speed
+            Features
+            Name
+            Rating
+            Upload_Speed
+            Ping
+            link
+            Type
+            Your_Name
+          }
+        }
+      }
+    }
+  }
+`
